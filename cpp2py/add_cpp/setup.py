8dc45b0066249c12bf4578ec44b7cccc3642345d
lusz <578752274@qq.com>
2024-06-22 17:56:09 +0800

    delete

--- cpp2py/add_cpp/setup.py
+++
@@ -1,9 +0,0 @@
-import torch
-from setuptools import setup
-from torch.utils.cpp_extension import CppExtension, BuildExtension
-
-setup(
-    name='add_cpp',
-    ext_modules=[CppExtension('add_cpp', ['add.cpp'])],
-    cmdclass={'build_ext': BuildExtension}
-)
