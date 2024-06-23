935cbffd1f9ef16862381fa144ec2324ca78c232
fzb <fzb0316@163.com>
2024-06-23 21:11:08 +0800

    modify test toundirected

--- jittor_geometric/tests/test_undirected.py
+++ jittor_geometric/tests/test_undirected.py
@@ -10,8 +10,9 @@ sys.path.append(os.path.dirname(os.path.dirname(os.path.dirname(os.path.abspath(
 from jittor_geometric.ops import toUndirected
 
 jt.flags.lazy_execution = 0
-edge_index = jt.array([[0, 1, 1],[1, 0, 2]])
-edge_attr = jt.array([1., 1., 1.])
+edge_index = jt.array([[0, 1, 1],
+                       [2, 0, 2]])
+edge_attr = jt.array([1., 3., 2.])
 num_edges=3
 num_nodes=3
 new_edge_index,new_edge_attr=toUndirected(edge_index,edge_attr,num_nodes)
