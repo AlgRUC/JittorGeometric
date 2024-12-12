eb983859dde92d7b3c126df73839a840c577c1aa
lusz <578752274@qq.com>
2024-12-12 15:14:00 +0800

    cusparse jittor

--- jittor_geometric/ops/spmmcsr.py
+++ jittor_geometric/ops/spmmcsr.py
@@ -11,9 +11,10 @@ from jittor import Function
 sys.path.append(os.path.dirname(os.path.dirname(os.path.dirname(os.path.abspath(__file__)))))
 from jittor_geometric.data import CSR
 module_path = os.path.dirname(__file__)
-src = os.path.join(module_path, "cpp/spmmcsr_op.cc")
-header = os.path.join(module_path, "cpp/spmmcsr_op.h")
-spmmcsr_op = jt.compile_custom_ops((src, header))
+# src = os.path.join(module_path, "cpp/spmmcsr_op.cc")
+# header = os.path.join(module_path, "cpp/spmmcsr_op.h")
+# spmmcsr_op = jt.compile_custom_ops((src, header))
+from jittor.compile_extern import cusparse_ops
 # Run the test
 jt.flags.use_cuda=1
 class SpmmCsrFunc(Function):
@@ -24,12 +25,14 @@ class SpmmCsrFunc(Function):
         self.v_num=v_num
         self.feature_dim=feature_dim
         output=jt.zeros(v_num,feature_dim)
-        spmmcsr_op.spmmcsr(output,x,csr.column_indices,csr.edge_weight,csr.row_offset,v_num,v_num).fetch_sync()
+        cusparse_ops.cusparse_spmmcsr(output,x,csr.column_indices,csr.edge_weight,csr.row_offset,v_num,v_num).fetch_sync()
+        # spmmcsr_op.spmmcsr(output,x,csr.column_indices,csr.edge_weight,csr.row_offset,v_num,v_num).fetch_sync()
         return output
 
     def grad(self, grad_output):
         output_grad=jt.zeros(self.v_num,self.feature_dim)
-        spmmcsr_op.spmmcsr(output_grad,grad_output,self.csr.column_indices,self.csr.edge_weight,self.csr.row_offset,self.v_num,self.v_num).fetch_sync()
+        cusparse_ops.cusparse_spmmcsr(output_grad,grad_output,self.csr.column_indices,self.csr.edge_weight,self.csr.row_offset,self.v_num,self.v_num).fetch_sync()
+        # spmmcsr_op.spmmcsr(output_grad,grad_output,self.csr.column_indices,self.csr.edge_weight,self.csr.row_offset,self.v_num,self.v_num).fetch_sync()
         return output_grad,None
     
 
