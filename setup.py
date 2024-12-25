d57a4af2e47b3e7d704577d077672b2fef1e1f20
m13965612473@gmail.com <m13965612473@gmail.com>
2024-12-25 14:07:14 +0800

    项目打包（支持c++文件,修改）

--- setup.py
+++ setup.py
@@ -1,21 +1,5 @@
 from setuptools import setup, find_packages, Extension
 
-module = Extension('cpp',
-                   sources=['jittor_geometric/ops/cpp/addone_op.cc',
-                            'jittor_geometric/ops/cpp/aggregate_op.cc',
-                            'jittor_geometric/ops/cpp/cootocsc_op.cc',
-                            'jittor_geometric/ops/cpp/cootocsr_op.cc',
-                            'jittor_geometric/ops/cpp/edgesoftmax_op.cc',
-                            'jittor_geometric/ops/cpp/edgesoftmaxbackward_op.cc',
-                            'jittor_geometric/ops/cpp/edgetovertex_op.cc',
-                            'jittor_geometric/ops/cpp/scattertoedge_op.cc',
-                            'jittor_geometric/ops/cpp/spmmcoo_op.cc',
-                            'jittor_geometric/ops/cpp/spmmcsr_op.cc',
-                            'jittor_geometric/ops/cpp/toundirected_op.cc',
-                            ],
-                   include_dirs=['jittor_geometric/ops/cpp'],
-                   language='c++',
-                   )
 
 setup(
     name='jittor_geometric',
@@ -23,6 +7,9 @@ setup(
     # author='Your Name',有一些信息不清楚发行版该怎么填，就先注释掉了
     # author_email='your.email@example.com',恳请大佬们写一下吧
     packages=find_packages(),
+    package_data={
+        'jittor_geometric': ['ops/cpp/*.cpp', 'ops/cpp/*.h'],
+    },
     # description='A brief description of the library',
     long_description=open('README.md').read(),
     long_description_content_type='text/markdown',
@@ -40,5 +27,4 @@ setup(
     #
     ],
     # python_requires='',
-    ext_modules=[module],
 )
