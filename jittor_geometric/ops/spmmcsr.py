6b538df6601b36bccdf2fe8bcef2c4edc66318d4
lusz <578752274@qq.com>
2024-12-29 15:06:16 +0800

    update spmm

--- jittor_geometric/ops/spmmcsr.py
+++ jittor_geometric/ops/spmmcsr.py
@@ -19,24 +19,26 @@ from jittor.compile_extern import cusparse_ops
 # Run the test
 jt.flags.use_cuda=1
 class SpmmCsrFunc(Function):
-    def execute(self,x,csr):
+    def execute(self,x,csr,trans_A,trans_B):
         self.csr=csr
         feature_dim=jt.size(x,1)        
         v_num=jt.size(csr.row_offset,0)-1
         self.v_num=v_num
         self.feature_dim=feature_dim
         output=jt.zeros(v_num,feature_dim)
-        cusparse_ops.cusparse_spmmcsr(output,x,csr.column_indices,csr.edge_weight,csr.row_offset,v_num,v_num).fetch_sync()
+        self.trans_A=trans_A
+        self.trans_B=trans_B
+        cusparse_ops.cusparse_spmmcsr(output,x,csr.column_indices,csr.edge_weight,csr.row_offset,v_num,v_num,trans_A,trans_B).fetch_sync()
         # spmmcsr_op.spmmcsr(output,x,csr.column_indices,csr.edge_weight,csr.row_offset,v_num,v_num).fetch_sync()
         return output
 
     def grad(self, grad_output):
         output_grad=jt.zeros(self.v_num,self.feature_dim)
-        cusparse_ops.cusparse_spmmcsr(output_grad,grad_output,self.csr.column_indices,self.csr.edge_weight,self.csr.row_offset,self.v_num,self.v_num).fetch_sync()
+        cusparse_ops.cusparse_spmmcsr(output_grad,grad_output,self.csr.column_indices,self.csr.edge_weight,self.csr.row_offset,self.v_num,self.v_num,self.trans_A,self.trans_B).fetch_sync()
         # spmmcsr_op.spmmcsr(output_grad,grad_output,self.csr.column_indices,self.csr.edge_weight,self.csr.row_offset,self.v_num,self.v_num).fetch_sync()
         return output_grad,None
     
 
-def SpmmCsr(x,csr):
-    out = SpmmCsrFunc.apply(x,csr)
+def SpmmCsr(x,csr,trans_A=True,trans_B=False):
+    out = SpmmCsrFunc.apply(x,csr,trans_A,trans_B)
     return out
