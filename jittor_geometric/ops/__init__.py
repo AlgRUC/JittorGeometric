465a395ac001ccd0e8d7a82d6b55ca266051bbf2
lusz <578752274@qq.com>
2025-01-04 14:20:16 +0800

    delete test op

--- jittor_geometric/ops/__init__.py
+++ jittor_geometric/ops/__init__.py
@@ -3,7 +3,6 @@ Author: lusz
 Date: 2024-06-21 11:01:25
 Description: 
 '''
-from .addone import addone
 from .aggregateWithWeight import aggregateWithWeight
 from .cootocsc import cootocsc
 from .cootocsr import cootocsr
