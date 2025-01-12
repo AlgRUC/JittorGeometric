4a08dfeb32592a2ea885efe4c4971d489c15c636
lusz <578752274@qq.com>
2025-01-12 16:47:25 +0800

    update fromto op

--- jittor_geometric/tests/test_fromtonodes.py
+++ jittor_geometric/tests/test_fromtonodes.py
@@ -19,7 +19,7 @@ col_indices=jt.array([0,2,1,4,0,1,3])
 csr_weight=None
 csr=CSR(col_indices, row_offset, csr_weight)
 
-nodes = jt.array([1, 2, 4])
+nodes = jt.array([1, 2, 2, 4])
 result1 = from_nodes(csc=csc, nodes=nodes)
 result2 = to_nodes(csr=csr ,nodes=nodes)
 print(result1) # 0 1 2 4
