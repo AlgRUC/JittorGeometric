9329e34dc5186e2bf53d3be76d7c298a74fbd3b0
lusz <578752274@qq.com>
2025-01-14 23:03:53 +0800

    merge

--- jittor_geometric/data/__init__.py
+++ jittor_geometric/data/__init__.py
@@ -1,17 +1,18 @@
+'''
+Author: lusz
+Date: 2025-01-14 00:47:42
+Description:
+'''
 from .data import Data
 from .dataset import Dataset
 from .in_memory_dataset import InMemoryDataset
 from .download import download_url, decide_download, extract_zip
 from .data import CSC,CSR
 from .temporal import TemporalData
-<<<<<<< HEAD
-from .graphchunk import GraphChunk
-=======
 from .batch import Batch
 from .dictionary import Dictionary
 from .conformer import ConformerGen
 # from .graphchunk import GraphChunk
->>>>>>> main
 __all__ = [
     'Data',
     'Dataset',
@@ -20,15 +21,9 @@ __all__ = [
     'decide_download',
     'extract_zip',
     'CSC',
-<<<<<<< HEAD
-    'CSR',
-    'TemporalData',
-    'GraphChunk'
-=======
     'CSR'
     'TemporalData',
     'Batch',
     'Dictionary',
     'ConformerGen',
->>>>>>> main
 ]
