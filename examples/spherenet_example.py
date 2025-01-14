11eaaea2ef2a2aa9d22e4859aad7bb025feb3489
zhangyang <fengyuewuya>
2025-01-14 12:48:50 +0800

    1. add spherenet with no_grad in eval

--- examples/spherenet_example.py
+++ examples/spherenet_example.py
@@ -43,20 +43,21 @@ def train(model, loader, optimizer):
 
 
 def eval(model, loader):
-    model.eval()
-    y_true = []
-    y_pred = []
+    with jt.no_grad:
+        model.eval()
+        y_true = []
+        y_pred = []
 
-    # batch_data.z, batch_data.pos, batch_data.pos
-    for step, batch_data in enumerate(tqdm(loader, desc="Iteration")):
-        pred = model(batch_data)
-        y_true.append(batch_data.y)
-        y_pred.append(pred)
+        # batch_data.z, batch_data.pos, batch_data.pos
+        for step, batch_data in enumerate(tqdm(loader, desc="Iteration")):
+            pred = model(batch_data)
+            y_true.append(batch_data.y)
+            y_pred.append(pred)
 
-    y_true = jt.cat(y_true, dim = 0)
-    y_pred = jt.cat(y_pred, dim = 0)
+        y_true = jt.cat(y_true, dim = 0)
+        y_pred = jt.cat(y_pred, dim = 0)
 
-    return float(mae_loss(y_pred, y_true))
+        return float(mae_loss(y_pred, y_true))
 
 def main():
     # data
