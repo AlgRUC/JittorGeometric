339e27e1ba3d7b54dbd09100bdb37404cf3e5eeb
lusz <578752274@qq.com>
2025-01-16 13:09:12 +0800

    update partition

--- jittor_geometric/partition/__init__.py
+++ jittor_geometric/partition/__init__.py
@@ -3,3 +3,5 @@ Description:
 Author: lusz
 Date: 2024-11-15 14:13:33
 '''
+from .chunk_manager import *
+from .partition_graph import *
--- jittor_geometric/partition/chunk_manager.py
+++ jittor_geometric/partition/chunk_manager.py
@@ -46,27 +46,28 @@ class ChunkManager:
 
     def partition_to_chunk(self, partition_file, edge_index, edge_weight, num_nodes, num_parts):
         """
-        将图划分为多个 chunk，并为每个 chunk 创建 GraphChunk 对象。
-        
-        :param partition_file: 包含分区信息的文件路径
-        :param edge_index: 边索引（两个数组分别表示边的起点和终点）
-        :param edge_weight: 边的权重
-        :param num_nodes: 图中顶点的总数
-        :param num_parts: 图划分的 chunk 数量
-        :return: GraphChunk 对象列表
+            Partition the graph into multiple chunks and create a GraphChunk object for each chunk.
+
+            :param partition_file: File path containing partition information
+            :param edge_index: Edge index (two arrays representing the start and end nodes of edges)
+            :param edge_weight: Weights of the edges
+            :param num_nodes: Total number of nodes in the graph
+            :param num_parts: Number of chunks to partition the graph into
+            :return: List of GraphChunk objects
         """
+
         with open(partition_file, 'rb') as f:
             partition = pickle.load(f)
         print(f"Partition file loaded from {partition_file}")
         
-        # 按分区重新映射顶点编号
+        # Remap vertex indices according to the partition
         sorted_indices = np.argsort(partition)
         remap = np.zeros(num_nodes, dtype=np.int64)
         remap[sorted_indices] = np.arange(num_nodes)
         edge_index[0] = remap[edge_index[0]]
         edge_index[1] = remap[edge_index[1]]
         
-        # 计算每个分区的起始和结束位置
+        # Calculate the start and end position for each partition
         partition_offset = [0]
         for part_id in range(num_parts):
             partition_offset.append(np.sum(partition == part_id) + partition_offset[-1])
@@ -76,14 +77,12 @@ class ChunkManager:
         
         for part_id in range(num_parts):
             start, end = partition_offset[part_id], partition_offset[part_id + 1]
-            chunk_nodes = range(start, end)  # 当前 chunk 包含的节点
+            chunk_nodes = range(start, end)   # Nodes included in the current chunk
             chunk_edges = np.isin(edge_index[0], chunk_nodes) & np.isin(edge_index[1], chunk_nodes)
-            
-            # 提取边信息
             chunk_edge_index = edge_index[:, chunk_edges]
             chunk_edge_weight = edge_weight[chunk_edges] if edge_weight is not None else None
             
-            # 创建 GraphChunk
+            # Create a GraphChunk object
             graph_chunk = GraphChunk(
                 chunks=num_parts,
                 chunk_id=part_id,
@@ -98,7 +97,6 @@ class ChunkManager:
             chunks.append(graph_chunk)
         
         return chunks
-        return
 
     @staticmethod
     def _edge_index_to_adj_list(edge_index, num_nodes):
